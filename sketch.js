b7d5414d847fee1153380083b5307d7918d05310
krushang patel <36439385+kushpatel544@users.noreply.github.com>
2022-02-26 02:14:17 -0600

    Changed Theme

--- sketch.js
+++ sketch.js
@@ -6,8 +6,8 @@ let board;
 let next;
 
 function setup() {
-  createCanvas(800,800);
-  w = 20;
+  createCanvas(windowWidth, windowHeight);
+  w = 10;
 
   columns = floor(width / w);
   rows = floor(height / w);
@@ -16,54 +16,59 @@ function setup() {
   for (let i = 0; i < columns; i++) {
     board[i] = new Array(rows);
   }
-  
+
   next = new Array(columns);
   for (i = 0; i < columns; i++) {
     next[i] = new Array(rows);
   }
+  frameRate(30)
   init();
 }
 
 function draw() {
-  background(255);
+  background(200);
   generate();
   for ( let i = 0; i < columns;i++) {
     for ( let j = 0; j < rows;j++) {
-      if ((board[i][j] == 1)) fill(0);
-      else fill(255);
-      stroke(0);
+      if ((board[i][j] == 1)){
+        fill(255);
+      }
+      else fill(0);
+      stroke(50);
       rect(i * w, j * w, w-1, w-1);
     }
   }
 
 }
 
-function keyPressed(){
-  init()
-  }
 
+function keyPressed(){
+    init();
+}
 function mousePressed() {
   init();
 }
 
+
 function init() {
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
-
+  
       if (i == 0 || j == 0 || i == columns-1 || j == rows-1) board[i][j] = 0;
-      
+     
       else board[i][j] = floor(random(2));
       next[i][j] = 0;
     }
   }
 }
 
+
 function generate() {
 
 
   for (let x = 1; x < columns - 1; x++) {
     for (let y = 1; y < rows - 1; y++) {
-     
+      
       let neighbors = 0;
       for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
@@ -71,9 +76,9 @@ function generate() {
         }
       }
 
-     
+   
       neighbors -= board[x][y];
-      
+      // Rules of Life
       if      ((board[x][y] == 1) && (neighbors <  2)) next[x][y] = 0;           // Loneliness
       else if ((board[x][y] == 1) && (neighbors >  3)) next[x][y] = 0;           // Overpopulation
       else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1;           // Reproduction
@@ -86,3 +91,6 @@ function generate() {
   next = temp;
 }
 
+function windowResized() {
+   resizeCanvas(windowWidth, windowHeight);
+}
